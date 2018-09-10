c107fc51ee9a7e2bcf69f76ca4d352910a3d9331
Abhishek1 <Abhishek1.sisgain@gmail.com>
2018-09-10 14:27:36 +0530

    change for mozilla browser

--- app/common/function/commonfunctions.js
+++ app/common/function/commonfunctions.js
@@ -1,5 +1,7 @@
 function isNumberKey(evt) {
 	var charCode = (evt.which) ? evt.which : event.keyCode;
+	 if (charCode == 8)
+        return true;
     if ((charCode < 48 || charCode > 57))
         return false;
 	return true;
