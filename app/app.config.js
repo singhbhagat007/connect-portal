28066d66f0f670266e349d48b338b5e9e2bc0489
pankaj_angular <pankaj1.sisgain@gmail.com>
2018-09-11 14:48:07 +0530

    change js file version in index.js and browser support issue

--- app/app.config.js
+++ app/app.config.js
@@ -1,4 +1,14 @@
-angular.module('AkosPCP').constant('appConfig', {
+angular.module('AkosPCP').constant('config', {
+
+    serverBaseUrl: 'https://sandbox.connect-api.akosmd.com',
+    socketBaseUrl: 'https://sandbox.connect-api.akosmd.com',
+    opentokAPIKey: 45732912
+    //serverBaseUrl : 'https://akosmd.com:3001',
+    // socketBaseUrl: 'https://akosmd.com:3001',
+    // serverBaseUrl: 'http://localhost:3001',
+    // opentokAPIKey: 45732912
+
+}).constant('appConfig', {
 
     employerId: '',
     network_check_provider: true,
@@ -9,14 +19,4 @@ angular.module('AkosPCP').constant('appConfig', {
     }
     
 }); 
-angular.module('AkosPCP').constant('config', {
 
-    serverBaseUrl : 'https://sandbox.connect-api.akosmd.com',
-    socketBaseUrl : 'https://sandbox.connect-api.akosmd.com',
-    opentokAPIKey : 45732912
-    //serverBaseUrl : 'https://akosmd.com:3001',
-    // socketBaseUrl: 'https://akosmd.com:3001',
-    // serverBaseUrl: 'http://localhost:3001',
-    // opentokAPIKey: 45732912
-
-}); 
