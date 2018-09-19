887fd0e303a25bb8d970a14018d14a39bc94dea8
pankaj_angular <pankaj1.sisgain@gmail.com>
2018-09-19 17:44:32 +0530

    change text nurse to provider andadd in config

--- app/app.config.js
+++ app/app.config.js
@@ -3,6 +3,7 @@ angular.module('AkosPCP').constant('config', {
     serverBaseUrl: 'https://sandbox.connect-api.akosmd.com',
     socketBaseUrl: 'https://sandbox.connect-api.akosmd.com',
     opentokAPIKey: 45732912
+   
     
 }).constant('appConfig', {
 
@@ -12,7 +13,9 @@ angular.module('AkosPCP').constant('config', {
        callDisconnected: { title: "Call disconnected by provider", message: "The call has been ended by your provider. Thank you for using Akos Connect video consultation." },
         notifyMessage: { title: "Doctor Notification", message: "An Akos connect video call is on the way. Keep an eye on your email/text for a video conferencing link to join the teleconference." },
         alreadyInConference: { title: "Already in the conference", message: "We are not able to initiate the video conference because you are already in another active video conference. Please try again later." },
-        callEndThanksMsg:{title:"",message:"Thank you for using Akos Teleclinic platform. Please inform the MA that your call has ended."}
+        callEndThanksMsg:{title:"",message:"Thank you for using Akos Teleclinic platform. Please inform the MA that your call has ended."},
+        joinCallMsgBysingle:{title:"",message:"Thanks for joining my personal consultation room. I am aware of your arrival in this waiting room and will be with you shortly."},
+        joinCallMsgBymultiple:{title:"",message:"Thanks for joining our consultation room. Our providers are aware of your arrival in this waiting room and they will be with you shortly."}
     }
     
 }); 
